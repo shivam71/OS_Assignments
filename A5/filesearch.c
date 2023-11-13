22dad47aa58601e6f5058cdeca13e5e17887382c
shivam71 <shivammadan@outlook.com>
2023-11-13 20:13:48 +0530

    Added comments

--- A5/filesearch.c
+++ A5/filesearch.c
@@ -6,22 +6,23 @@
 #include<sys/stat.h>
 
 char* target;
-char path[256];
+char path[256];// For storing the path 
 int type; // 0 file 1 dir and 2 link 
 
-int find_type(mode_t entry_mode){
-	if(S_ISDIR(entry_mode)){
+int find_type(mode_t entry_mode){ // using the file stats determines the type
+	if(S_ISDIR(entry_mode)){//Check directory 
 		return 1;
-        }else if(S_ISREG(entry_mode)){
+        }else if(S_ISREG(entry_mode)){//Check Regular file
 		return 0;
-        }else if(S_ISLNK(entry_mode)){
+        }else if(S_ISLNK(entry_mode)){//Check if link 
 		return 2;
         }else{
-		return -1;
+		return -1;// Any other
 	}
 }
 
 bool search_directory(){
+	// Recursively searches for the target in the current directory 
 	DIR* cdir_ptr = opendir(".");
 	struct dirent* entry;
 	struct stat entry_stats;
@@ -33,18 +34,18 @@ bool search_directory(){
 		if(strcmp(".",entry->d_name)==0 || strcmp("..",entry->d_name)==0){
 			continue;
 		}
-		lstat(entry->d_name,&entry_stats);
+		lstat(entry->d_name,&entry_stats);// get the stats of the target 
 		entry_mode = entry_stats.st_mode;
 		entry_type = find_type(entry_mode);
 		if(entry_type==-1) continue;
-		if(strcmp(entry->d_name,target)==0){
+		if(strcmp(entry->d_name,target)==0){// if the name of the entry matches target then store the path and return 
 			getcwd(path,256);
 			type = entry_type;
 			return true;
 		}
-		// recursively search the sub directory 
+		// recursively search the sub directory in case the entry is a directory 
 		if(S_ISDIR(entry_mode)){
-			chdir(entry->d_name);
+			chdir(entry->d_name);// Enter that directory 
 			found = search_directory();
 			chdir("..");// return back to the cdir
 			if(found) return true;
@@ -57,10 +58,10 @@ bool search_directory(){
 
 int main(int argc,char *argv[]){
 	if(argc!=2){
-		printf("Error\n");
+		printf("Error wrong input just pass the target name no other string \n");
 		return 1;
 	}
-	target = argv[1];
+	target = argv[1];// Traget name
 	if(search_directory()){
 		if(type==0){
 			printf("File found: %s/%s\n",path,target);
@@ -70,7 +71,7 @@ int main(int argc,char *argv[]){
 			printf("Link found: %s/%s\n",path,target);
 		}
 	}else{
-		printf("The target %s was not found in the current directory\n",target);
+		printf("The target '%s' was not found in the current directory\n",target);
 	}
 	return 0;
 }
